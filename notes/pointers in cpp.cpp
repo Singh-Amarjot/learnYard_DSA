8e439663ef137572a2bb04c0800d7d396556965b
Singh-Amarjot <jotbhari6645@gmail.com>
2024-01-26 20:40:33 +0530

    pointers info updated

--- notes/pointers in cpp.cpp
+++ notes/pointers in cpp.cpp
@@ -24,5 +24,29 @@ int main() {
 
     // the data type of ptr is int exacty same as the variable whose address its going to hold but why ?
     // this is because ......... do some research ..
+
+    // pointer t pointer 
+
+    // now , as pointer also would need some memory itself so we can store the memory adress of a pointer in another ponter called a pointer 2 pointer 
+
+    // like this 
+    int var = 10;
+    int *ptr = &var; // pointer hoding the memory address of variable var 
+    int **ptr_ptr = &ptr ;// pointer holding the memory addresss of the pointer variable which further holds the memory address of a variable called var  
+
+
+
+    // so , in this way
+
+   cout<<ptr<<endl; // prints the adress of variable var 
+   cout<<*ptr<<endl; // dereferences the address and prints the actual value in var 
+   cout<<ptr_ptr<<endl; // prints the memor addrsess of the pointer ptr 
+   cout<<*ptr_ptr<<endl; // prits the value being held by the pointer ptr that is the address of var 
+   cout<<**ptr_ptr<<endl; // prnts the value of var 
+
+
+
+
+
     return 0;
 }
